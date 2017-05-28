2079122c61e0559ca5f0aa1ca9e5a5463ca48bcb
zhangzhibo <columbia@163.com>
2017-05-28 16:54:19 +0800

    2017-05-28 16:54

--- src/main/java/org/conan/myhadoop02/mr/chap05/ConfigurationPrinter.java
+++ src/main/java/org/conan/myhadoop02/mr/chap05/ConfigurationPrinter.java
@@ -1,11 +1,35 @@
 package org.conan.myhadoop02.mr.chap05;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.util.Map;
+
 /**
  * Created by zhangzhibo on 17-5-28.
  */
-public class ConfigurationPrinter {
-    
-    public static void main(String[] args) {
+public class ConfigurationPrinter extends Configured implements Tool {
+    static {
+        Configuration.addDefaultResource("hdfs-default.xml");
+        Configuration.addDefaultResource("hdfs-sit.xml");
+        Configuration.addDefaultResource("mapred-default.xml");
+        Configuration.addDefaultResource("mapred-sit.xml");
+    }
 
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = getConf();
+        for (Map.Entry<String, String> entry :
+                conf) {
+            System.out.printf("%s=%s\n", entry.getKey(), entry.getValue());
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new ConfigurationPrinter(), args);
+        System.exit(exitCode);
     }
 }
