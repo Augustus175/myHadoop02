99391ba706702bb9356ec60ad34139689a0ddd40
zhangzhibo <columbia@163.com>
2017-06-21 22:51:57 +0800

    2017-06-21 21:54

---
+++ src/main/java/org/conan/myhadoop02/mr/chap08/SortByTemperatureUsingHashPartitioner.java
@@ -0,0 +1,38 @@
+package org.conan.myhadoop02.mr.chap08;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.conan.myhadoop02.mr.JobBuilder;
+
+/**
+ * Created by zhangzhibo on 17-6-21.
+ */
+public class SortByTemperatureUsingHashPartitioner extends Configured implements Tool {
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = JobBuilder.parseInputAndOutput(this, getConf(), args);
+        if (job == null) {
+            return -1;
+        }
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        SequenceFileOutputFormat.setCompressOutput(job, true);
+        SequenceFileOutputFormat.setOutputCompressorClass(job,GzipCodec.class);
+        SequenceFileOutputFormat.setOutputCompressionType(job, SequenceFile.CompressionType.BLOCK);
+        return job.waitForCompletion(true)? 0:1;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new SortDataPreprocessor(), args);
+        System.exit(exitCode);
+    }
+}
