1c1d66fd013ad1f9bc52e4ca936376f3f519af7e
zhangzhibo <columbia@163.com>
2017-07-18 07:26:25 +0800

    2017-07-17 21:54

--- src/main/java/org/conan/myhadoop02/mr/chap13/ExampleClient.java
+++ src/main/java/org/conan/myhadoop02/mr/chap13/ExampleClient.java
@@ -6,9 +6,7 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.eclipse.jdt.internal.core.SourceType;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 /**
