d63fd69f1481a64fa8711e1770f230c94b5fafcf
zhangzhibo <columbia@163.com>
2017-07-18 07:25:09 +0800

    2017-07-17 21:54

--- src/main/java/org/conan/myhadoop02/mr/chap13/ExampleClient.java
+++ src/main/java/org/conan/myhadoop02/mr/chap13/ExampleClient.java
@@ -16,7 +16,9 @@ import java.io.IOException;
  */
 public class ExampleClient {
     public static void main(String[] args) throws IOException {
-        Configuration config = HBaseConfiguration.create();
+        Configuration conf = new Configuration();
+        conf.addResource("/usr/local/hbase/conf/hbase-site-cluster.xml");
+        Configuration config = HBaseConfiguration.create(conf);
         HBaseAdmin admin = new HBaseAdmin(config);
         HTableDescriptor htd = new HTableDescriptor("test");
         HColumnDescriptor hcd = new HColumnDescriptor("data");
@@ -24,25 +26,25 @@ public class ExampleClient {
         admin.createTable(htd);
         byte[] tablename = htd.getName();
         HTableDescriptor[] tables = admin.listTables();
-        if (tables.length!=1&& Bytes.equals(tablename,tables[0].getName())){
+        if (tables.length != 1 && Bytes.equals(tablename, tables[0].getName())) {
             throw new IOException("Failed create of table");
         }
-        HTable table = new HTable(config,tablename);
+        HTable table = new HTable(config, tablename);
         byte[] row1 = Bytes.toBytes("row1");
         Put p1 = new Put(row1);
         byte[] databytes = Bytes.toBytes("data");
-        p1.add(databytes,Bytes.toBytes("1"),Bytes.toBytes("value1"));
+        p1.add(databytes, Bytes.toBytes("1"), Bytes.toBytes("value1"));
         table.put(p1);
         Get g = new Get(row1);
         Result result = table.get(g);
-        System.out.println("Get : "+ result);
+        System.out.println("Get : " + result);
         Scan scan = new Scan();
         ResultScanner scanner = table.getScanner(scan);
-        try{
-            for (Result scannerResult : scanner){
-                System.out.println("Scan: "+scannerResult);
+        try {
+            for (Result scannerResult : scanner) {
+                System.out.println("Scan: " + scannerResult);
             }
-        }finally {
+        } finally {
             scanner.close();
         }
         admin.disableTable(tablename);
